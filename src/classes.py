26ce483869ec785a6e09eeb0f75c2d14c699baf7
VikVyaz <gluckundhappy@gmail.com>
2025-01-06 22:08:42 +0300

    classes.py, cov and fixes

---
+++ src/classes.py
@@ -0,0 +1,35 @@
+class Product:
+    """
+    Класс для предоставления продукта
+    """
+
+    name: str
+    description: str
+    price: int | float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class Category:
+    """
+    Класс для предоставления категории
+    """
+
+    name: str
+    description: str
+    products: list[Product]
+
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products=None):
+        self.name = name
+        self.description = description
+        self.products = products if products else []
+        Category.category_count += 1
+        Category.product_count += len(products) if products else 0
